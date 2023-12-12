fea9f766637168f9145830c840d63ed12792d738
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-12 14:20:36 +0100

    add array di oggetti

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 /*
+Rifare l’esercizio della to do list.
+Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
+MILESTONE 1
+Stampare all’interno di una lista HTML un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 */
 
 const { createApp } = Vue
@@ -7,7 +20,20 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!',
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ]
       }
     }
   }).mount('#app')
