414e03673ed4dc414aa41531855e8090ac9d89f7
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-12 15:11:20 +0100

    add methods

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 
 MILESTONE 1
 Stampare all’interno di una lista HTML un item per ogni todo. FATTO
-Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato. FATTO
 
 MILESTONE 2
 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
@@ -39,5 +39,8 @@ const { createApp } = Vue
     },
     mounted () {
         console.log(this.todos[this.activeItem].text);
+    },
+    methods: {
+         
     }
   }).mount('#app')
